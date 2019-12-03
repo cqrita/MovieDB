4c2575888369bfa5d3e45ce3793fb3f0f1aee24e
cqrita <cqrita@naver.com>
2019-12-03 16:38:17 +0900

    commit

--- MovieDB/app/src/main/java/com/example/MovieDB/toolbar/MenuBar.java
+++ MovieDB/app/src/main/java/com/example/MovieDB/toolbar/MenuBar.java
@@ -1,10 +1,44 @@
 package com.example.MovieDB.toolbar;
 
 import android.content.Context;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
+import com.example.MovieDB.R;
+
+import butterknife.BindView;
+
 public class MenuBar extends LinearLayout {
+    @BindView(R.id.button1)
+    private Button button1;
+    @BindView(R.id.button2)
+    private Button button2;
+    @BindView(R.id.button3)
+    private Button button3;
+    private int getMenu=1;
     public MenuBar(Context context) {
         super(context);
+        button1.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getMenu=1;
+            }
+        });
+        button2.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getMenu=2;
+            }
+        });
+        button3.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getMenu=3;
+            }
+        });
+    }
+    public int getMenu(){
+        return this.getMenu;
     }
 }
