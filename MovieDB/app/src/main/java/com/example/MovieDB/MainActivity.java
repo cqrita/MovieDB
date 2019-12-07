ac8c12005ac4cd5023d57a81fd2bf51677115a74
user <cqrita@naver.com>
2019-12-07 23:01:26 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml
    #	MovieDB/.gradle/5.4.1/executionHistory/executionHistory.bin
    #	MovieDB/.gradle/5.4.1/executionHistory/executionHistory.lock
    #	MovieDB/.gradle/5.4.1/fileHashes/fileHashes.bin
    #	MovieDB/.gradle/5.4.1/fileHashes/fileHashes.lock
    #	MovieDB/.gradle/buildOutputCleanup/buildOutputCleanup.lock
    #	MovieDB/.gradle/buildOutputCleanup/outputFiles.bin
    #	MovieDB/.idea/caches/build_file_checksums.ser
    #	MovieDB/.idea/caches/gradle_models.ser
    #	MovieDB/.idea/libraries/Gradle__androidx_appcompat_appcompat_1_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__androidx_arch_core_core_runtime_2_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__androidx_asynclayoutinflater_asynclayoutinflater_1_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__androidx_cardview_cardview_1_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__androidx_coordinatorlayout_coordinatorlayout_1_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__androidx_core_core_1_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__androidx_cursoradapter_cursoradapter_1_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__androidx_customview_customview_1_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__androidx_documentfile_documentfile_1_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__androidx_drawerlayout_drawerlayout_1_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__androidx_fragment_fragment_1_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__androidx_interpolator_interpolator_1_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__androidx_legacy_legacy_support_core_ui_1_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__androidx_legacy_legacy_support_core_utils_1_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__androidx_legacy_legacy_support_v4_1_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__androidx_lifecycle_lifecycle_livedata_2_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__androidx_lifecycle_lifecycle_livedata_core_2_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__androidx_lifecycle_lifecycle_runtime_2_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__androidx_lifecycle_lifecycle_viewmodel_2_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__androidx_loader_loader_1_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__androidx_localbroadcastmanager_localbroadcastmanager_1_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__androidx_media_media_1_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__androidx_print_print_1_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__androidx_recyclerview_recyclerview_1_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__androidx_slidingpanelayout_slidingpanelayout_1_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__androidx_swiperefreshlayout_swiperefreshlayout_1_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__androidx_transition_transition_1_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__androidx_vectordrawable_vectordrawable_1_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__androidx_vectordrawable_vectordrawable_animated_1_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__androidx_versionedparcelable_versionedparcelable_1_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__androidx_viewpager_viewpager_1_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__com_github_mancj_MaterialSearchBar_0_8_2_aar.xml
    #	MovieDB/.idea/libraries/Gradle__com_google_android_material_material_1_0_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__com_jakewharton_butterknife_8_5_1_aar.xml
    #	MovieDB/.idea/libraries/Gradle__com_melnykov_floatingactionbutton_1_3_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__com_mikepenz_fontawesome_typeface_4_4_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__com_mikepenz_iconics_core_1_7_5_aar.xml
    #	MovieDB/.idea/libraries/Gradle__com_mikepenz_materialdrawer_4_0_5_aar.xml
    #	MovieDB/.idea/libraries/Gradle__com_mikepenz_materialize_0_2_0_aar.xml
    #	MovieDB/.idea/libraries/Gradle__com_yqritc_recyclerview_flexibledivider_1_2_5_aar.xml
    #	MovieDB/app/app.iml
    #	MovieDB/app/build/outputs/logs/manifest-merger-debug-report.txt

--- MovieDB/app/src/main/java/com/example/MovieDB/MainActivity.java
+++ MovieDB/app/src/main/java/com/example/MovieDB/MainActivity.java
@@ -4,17 +4,16 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SearchView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import com.example.MovieDB.fragment.FavoriteFragment;
 import com.example.MovieDB.fragment.HomeFragment;
 import com.example.MovieDB.fragment.SearchFragment;
-import com.example.MovieDB.toolbar.MenuBar;
-import com.example.MovieDB.toolbar.SearchBar;
-import com.google.android.material.button.MaterialButton;
 
 import java.util.List;
 
@@ -22,22 +21,18 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private List<String> lastSearches;
     private DrawerLayout drawer;
-//    @BindView(R.id.Msearchbar)
-//    private SearchBar searchBar;
-    private MenuBar menuBar;
+    private Spinner spinner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d("MainACtivity", "Seungrok");
         setContentView(R.layout.activity_main);
-//        searchBar= new SearchBar(this);
-        menuBar =new MenuBar(this);
-
         Button button1 = findViewById(R.id.button1);
         Button button2 = findViewById(R.id.button2);
         Button button3 = findViewById(R.id.button3);
-
+        SearchView searchView = findViewById(R.id.searchView);
         //시작 프레그먼트 지정
         HomeFragment homeFragment = new HomeFragment();
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
@@ -73,6 +68,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(MainActivity.this,"Our word : ",Toast.LENGTH_SHORT).show();
+                SearchFragment search= new SearchFragment();
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.add(R.id.fragment, search);
+                ft.commit();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                SearchFragment search= new SearchFragment();
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.add(R.id.fragment, search);
+                ft.commit();
+                return false;
+            }
+        });
     }
     @Override
     protected void onDestroy() {
--- MovieDB/app/src/main/java/com/example/MovieDB/adapter/MovieListAdapter.java
+++ MovieDB/app/src/main/java/com/example/MovieDB/adapter/MovieListAdapter.java
@@ -4,36 +4,38 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
 import com.example.MovieDB.R;
+import com.example.MovieDB.data.Movie;
+
+import java.util.ArrayList;
 
 
 /**
  * @author arun
  */
 public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.MyViewHolder >{
-//    private List<Movie> movies;
-    private String[] textSet;
-//    private Context context;
-    private String[] imgSet;
+    private ArrayList movieList;
+    private Context context;
 
-    public MovieListAdapter (String[] textSet,String[] imgSet ){
-        this.textSet = textSet;
-        this.imgSet = imgSet;
+    public MovieListAdapter (Context context, ArrayList movieList){
+        this.context = context;
+        this.movieList = movieList;
     }
 
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
-        public TextView moviePoster;
+        public ImageView moviePoster;
         public TextView movieName;
 
         public MyViewHolder(View view) {
             super(view);
-
-            // 뷰 객체에 대한 참조. (hold strong reference)
             this.moviePoster = view.findViewById(R.id.movie_poster);
             this.movieName = view.findViewById(R.id.movie_title);
         }
@@ -62,56 +64,24 @@ public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.MyVi
 //            this.movies = movies;
 //        }
         }
-        @Override
-        public MyViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-            Context context = viewGroup.getContext();
-            View holderView= LayoutInflater.from(context).inflate(R.layout.movie_item, viewGroup,
-                    false);
-
-            return new MyViewHolder(holderView);
-        }
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        Context context = viewGroup.getContext();
+        View holderView= LayoutInflater.from(context).inflate(R.layout.movie_item, viewGroup,
+                false);
+        return new MyViewHolder(holderView);
+    }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
-        myViewHolder.moviePoster.setText(imgSet[i]);
-        myViewHolder.movieName.setText(textSet[i]);
-
+        Movie movie = (Movie) movieList.get(i);
+        Glide.with(context).load("https://image.tmdb.org/t/p/"+movie.getPosterPath()).into(myViewHolder.moviePoster);
+        myViewHolder.movieName.setText(movie.getTitle());
     }
 
-//    @Override
-//        public void onBindViewHolder(MyViewHolder myViewholder, int i) {
-//
-//            //    holder.itemView.setOnClickListener(holder);
-//
-//            myViewholder.moviePoster.setText(imgSet[i]);
-//
-//
-//
-//
-//            holder. = movies.get(position);
-//            holder.name.setText(holder.movie.getTitle());
-//
-//            RequestOptions options = new RequestOptions()
-//                    .centerCrop()
-//                    .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
-//                    .priority(Priority.HIGH);
-//
-//            Glide.with(______)
-//                    .asBitmap()
-//                    .load(Api.getPosterPath(holder.movie.getPosterPath()))
-//                    .apply(options)
-//                    .into(new BitmapImageViewTarget(holder.poster) {
-//                        @Override
-//                        public void onResourceReady(Bitmap bitmap, @Nullable Transition<? super Bitmap> transition) {
-//                            super.onResourceReady(bitmap, transition);
-//                            Palette.from(bitmap).generate(palette -> setBackgroundColor(palette, holder));
-//                        }
-//                    });
-//
-
-        @Override
-        public int getItemCount() {
-            return textSet.length > imgSet.length ? textSet.length : imgSet.length;
-        }
+    @Override
+    public int getItemCount() {
+        return movieList.size();
     }
+}
 
