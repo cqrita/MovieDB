f26d5988973eb182b6ce8f77b434bd97827bf79c
Seungrok Yoon <44149596+SeungrokYoon@users.noreply.github.com>
2019-12-10 11:26:04 +0900

    test

--- MovieDB/app/src/main/java/com/example/MovieDB/database/FavoriteList.java
+++ MovieDB/app/src/main/java/com/example/MovieDB/database/FavoriteList.java
@@ -7,12 +7,16 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class FavoriteList extends SQLiteOpenHelper {
-    public FavoriteList(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    private static final String DATABASE_NAME = "favorite_movie.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public FavoriteList(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE_NOTE);
 
     }
 
