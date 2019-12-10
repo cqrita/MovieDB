25d043d6366a7539170a37b194c3f04924f246e8
Seungrok Yoon <44149596+SeungrokYoon@users.noreply.github.com>
2019-12-10 12:58:06 +0900

    FAVORITE UPDATE

--- MovieDB/app/src/main/java/com/example/MovieDB/fragment/FavoriteFragment.java
+++ MovieDB/app/src/main/java/com/example/MovieDB/fragment/FavoriteFragment.java
@@ -1,10 +1,12 @@
 package com.example.MovieDB.fragment;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -13,10 +15,18 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.MovieDB.R;
 import com.example.MovieDB.adapter.MovieListAdapter;
+import com.example.MovieDB.database.FavoriteDBHelper;
 
 import java.util.ArrayList;
 
 public class FavoriteFragment extends Fragment {
+    ImageView imageView;
+    private SQLiteDatabase mDb;
+    private FavoriteDBHelper favoriteDbHelper;
+    private RecyclerView recyclerView;
+
+
+
     public FavoriteFragment() {
 
     }
@@ -24,20 +34,21 @@ public class FavoriteFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         Log.d("fragment","Favorite Fragment loaded");
-//        // 리사이클러뷰에 표시할 데이터 리스트 생성.
-//        String[] list = new String[100];
-//        for (int i=0; i<100; i++) {
-//            list[i] = "1";
-//        }
-//        // 리사이클러뷰에 LinearLayoutManager 객체 지정.
-//        //fragment 에서는 Getview 를 통해서 fragment root 의 view 를 받아온다.
-//        RecyclerView recyclerView = getView().findViewById(R.id.home_recycler_view) ;
-//        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity())) ;//해당 fragment 를
-//        // 관리하는 avcivity를 리턴하는 함수인 getActivity()
-//
-//        // 리사이클러뷰에 SimpleTextAdapter 객체 지정.
-//        MovieListAdapter adapter = new MovieListAdapter(list, list) ;
-//        recyclerView.setAdapter(adapter) ;
+
+     //DB사용하여 불러오기
+        FavoriteDBHelper dbHelper = new FavoriteDBHelper(this);
+        mDb = dbHelper.getWritableDatabase();
+
+
+
+
+
+
+
+
+
+
+
         return inflater.inflate(R.layout.favorite, container, false);
 
     }
