5b0fc6c96b7ee35e0ebaacf6b05c783ce7e5a2b1
Seungrok Yoon <44149596+SeungrokYoon@users.noreply.github.com>
2019-12-17 09:35:17 +0900

    정리정돈

--- MovieDB/app/src/main/java/com/example/MovieDB/fragment/SearchFragment.java
+++ MovieDB/app/src/main/java/com/example/MovieDB/fragment/SearchFragment.java
@@ -16,6 +16,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.MovieDB.R;
 import com.example.MovieDB.adapter.MovieListAdapter;
+import com.example.MovieDB.data.Api;
 import com.example.MovieDB.data.Movie;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
@@ -129,8 +130,12 @@ public class SearchFragment extends Fragment {
         @Override
         protected Movie[] doInBackground(String... strings) {
             OkHttpClient client = new OkHttpClient();
+            Api api = new Api();
+            String api_key = api.getApikey2();
             Request request = new Request.Builder()
-                    .url("https://api.themoviedb.org/3/search/movie?api_key=ee74e4df4dd623e8eb831f2fd274328f&language=ko-KR&query="+string+"&page="+page)
+                    .url("https://api.themoviedb.org/3/search/movie?api_key=" + api_key +
+                            "&language=ko-KR" +
+                            "&query=" + string + "&page=" + page)
                     .build();
             try {
                 Response response = client.newCall(request).execute();
