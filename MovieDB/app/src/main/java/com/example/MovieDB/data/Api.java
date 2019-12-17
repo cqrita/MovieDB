922cddf5dcd15684cda3512371d9a7c24bb64d4d
Seungrok Yoon <44149596+SeungrokYoon@users.noreply.github.com>
2019-12-17 09:24:13 +0900

    Appkey home fragment만 수정함 xml은 못했습니다.

---
+++ MovieDB/app/src/main/java/com/example/MovieDB/data/Api.java
@@ -0,0 +1,17 @@
+package com.example.MovieDB.data;
+
+public class Api {
+    //Google
+    String apikey1 = "AIzaSyAFdyErPaOWanCHYnsUoqkuwi13fKNzykE";
+    //MovieDB
+    String apikey2 = "ee74e4df4dd623e8eb831f2fd274328f";
+
+    public static final String getApikey1() {
+        return apikey1;
+    }
+
+    public static final String getApikey2() {
+        return apikey2;
+    }
+
+}
